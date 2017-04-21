555760a64335b7efb5dae9a46d42e7bb4ac25d6f
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-04-21 09:16:48 +0530

    Data and modifications

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
-from flask import Flask, jsonify, request,abort
-
+from flask import Flask, jsonify, request,abort, make_response
+import json
 app = Flask(__name__)
 events = ['one','two','three']
 tasks = [
     {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'id': 'lel1',
+        'title': u'Whittaker vs Souza',
+        'prediction': u'Souza',
         'done': False
     },
     {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
+        'id': 'lel2',
+        'title': u'Mcgregor vs Mayweather',
+        'description': u'Mcgregor',
         'done': False
     }
 ]
+
+with open('Data/winners.json') as json_data:
+  d = json.load(json_data)
+  print d
+
 @app.route('/')
 def index():
   return "STILL HERE!"
 
-@app.route('/api/v1.0/gettask', methods=['GET'])
-def get_tasks():
-  fightid = request.args.get('fightid')
-  return jsonify({'tasks': tasks})
+@app.route('/api/v1.0/getprediction/<task_id>', methods=['GET'])
+def get_tasks(task_id):
+  task = [task for task in tasks if task['id']==task_id]
+  if(len(task)==0):
+    abort(404)
+  else:
+    return jsonify({'task':task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'STATUS': 'NOT OK', 'reason':'404'}), 404)
 
 if __name__ == '__main__':
   app.run(debug=True)
